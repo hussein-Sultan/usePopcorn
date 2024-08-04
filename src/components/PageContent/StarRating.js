ea7ca0300f7104b633b6f128c100dc38940f71ab
husseinCode <programmingadvices513@gmail.com>
2024-08-04 11:17:17 +0300

    testing

--- src/components/PageContent/StarRating.js
+++ src/components/PageContent/StarRating.js
@@ -31,6 +31,7 @@ export default function StarRating({
   className = "",
   onSetRating,
   defaultRating = 0,
+  test
 }) {
   const defaultRatingState = defaultRating <= maxRating ? defaultRating : 0;
 
