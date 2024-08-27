8c3f766ac3c1523653c1bdac629ffe8a0e9dc9d5
husseinCode <147065912+hussein-Sultan@users.noreply.github.com>
2024-08-27 18:39:17 +0300

    add Enter key feature, when you press enter key, the search query will focus

--- src/components/NavBar/SearchBar.js
+++ src/components/NavBar/SearchBar.js
@@ -1,6 +1,26 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function SearchBar({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      const callback = (e) => {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      };
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       type="text"
@@ -8,6 +28,7 @@ export default function SearchBar({ query, setQuery }) {
       onChange={(e) => setQuery(e.target.value)}
       className="search"
       placeholder="Search movies..."
+      ref={inputEl}
     />
   );
 }
